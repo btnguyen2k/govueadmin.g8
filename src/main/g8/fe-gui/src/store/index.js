a66a23699a313e92e48fc72a85553f788514f19e
Thanh Nguyen <thanhn@microsoft.com>
2022-06-05 19:58:31 +1000

    Migrate folder /src/store

---
+++ src/main/g8/fe-gui/src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    sidebarVisible: '',
+    sidebarUnfoldable: false,
+  },
+  mutations: {
+    toggleSidebar(state) {
+      state.sidebarVisible = !state.sidebarVisible
+    },
+    toggleUnfoldable(state) {
+      state.sidebarUnfoldable = !state.sidebarUnfoldable
+    },
+    updateSidebarVisible(state, payload) {
+      state.sidebarVisible = payload.value
+    },
+  },
+  actions: {},
+  modules: {},
+})
