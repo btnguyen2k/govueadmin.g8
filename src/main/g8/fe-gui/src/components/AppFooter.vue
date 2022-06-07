f25744ce35e9bf084118546c4a2a9caf65523c01
Thanh Nguyen <thanhn@microsoft.com>
2022-06-07 14:23:15 +1000

    Migrate page footer

--- src/main/g8/fe-gui/src/components/AppFooter.vue
+++ src/main/g8/fe-gui/src/components/AppFooter.vue
@@ -1,20 +1,29 @@
+<!-- #GovueAdmin-Customized -->
 <template>
   <CFooter>
-    <div>
-      <a href="https://coreui.io" target="_blank">CoreUI</a>
-      <span class="ms-1"
-        >&copy; {{ new Date().getFullYear() }} creativeLabs.</span
-      >
-    </div>
+    <div>© {{ appName }} version {{ appVersion }}.</div>
     <div class="ms-auto">
-      <span class="me-1" target="_blank">Powered by</span>
-      <a href="https://coreui.io/vue">CoreUI for Vue</a>
+      <span class="mr-1">Powered by </span
+      ><a href="https://github.com/btnguyen2k/govueadmin.g8" target="_blank"
+        >GoVueAdmin</a
+      >
+      |
+      <span class="mr-1">Template by </span
+      ><a href="https://coreui.io/vue" target="_blank">CoreUI for Vue</a>
     </div>
   </CFooter>
 </template>
 
 <script>
+import cfg from '@/utils/app_config'
+
 export default {
   name: 'AppFooter',
+  setup() {
+    return {
+      appName: cfg.APP_CONFIG.app.name,
+      appVersion: cfg.APP_CONFIG.app.version,
+    }
+  },
 }
 </script>
--- src/main/g8/fe-gui/src/containers/TheFooter.vue
+++ src/main/g8/fe-gui/src/containers/TheFooter.vue
@@ -3,9 +3,13 @@
   <CFooter :fixed="false">
     <div>© {{ appName }} version {{ appVersion }}.</div>
     <div class="mfs-auto">
-      <span class="mr-1">Powered by</span><a href="https://github.com/btnguyen2k/govueadmin.g8" target="_blank">GoVueAdmin</a>
+      <span class="mr-1">Powered by</span
+      ><a href="https://github.com/btnguyen2k/govueadmin.g8" target="_blank"
+        >GoVueAdmin</a
+      >
       |
-      <span class="mr-1">Template by</span><a href="https://coreui.io/vue" target="_blank">CoreUI for Vue</a>
+      <span class="mr-1">Template by</span
+      ><a href="https://coreui.io/vue" target="_blank">CoreUI for Vue</a>
     </div>
   </CFooter>
 </template>
